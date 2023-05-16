4fe89d69593272792470557259b007867eb759dd
Bea Mungcal <beamungcal@hotmail.com>
2023-05-15 20:41:31 -0400

    Included endpoint accessing urlDatabase

--- express_server.js
+++ express_server.js
@@ -4,10 +4,17 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs")
 
+//Used to keep track of all the URLs and their shortened forms
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//res.render("urls_index", templateVars) will take info from urls_index and show it in the browser. In this case, the templateVars, which is an object containing an object.
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
